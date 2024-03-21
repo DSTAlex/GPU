67ef504c0e330bc02bb93947ddcb8c64801fac5f
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-03-21 11:33:22 +0100

    bridged

--- bridge.sh
+++ bridge.sh
@@ -6,14 +6,15 @@ then
     git status
     git commit -m bridged
     git push
-elif [ $# -eq 1 ]
+elif [ $1 -eq "-c" ]
 then
     name=$dir_path$1
     echo "compile $name" 
     nvcc $name --allow-unsupported-compiler -o exo
     ./exo
     rm exo
-else
+elif [ $1 -eq "-b" ]
+then
     ssh -X -l alexandre.di-santo -p 2200$2 gpgpu.image.lrde.iaas.epita.fr
 fi
 
