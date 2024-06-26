f77b1056cf45f3c60d52dd6e96c00f66e84372cc
alexandre.di-santo <alexandre.di-santo@epiat.fr>
2024-06-26 15:31:14 +0200

    bridged

--- exam/ex2.cu
+++ exam/ex2.cu
@@ -101,8 +101,9 @@ void matvecmul3(const int* A, const int* b, int* c, int N, int M)
 
         for (int k = 0; k < T; k++)
         {
-           c[i % N] += s_B[k]; //s_A[i / N][k]; // * s_B[k];
+           c[i % N] += s_A[i / N][k] * s_B[k];
         }
+        c[i % N] = s_A[i / N][0];
         __syncthreads();
     }
 }
