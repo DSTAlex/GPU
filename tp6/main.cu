5c5d5c94745825721c0bfa2d16cf93e58b65a7bd
alexandre di-santo <alexandre.di-santo@epita.fr>
2024-11-04 10:22:19 +0100

    bridged

--- tp6/main.cu
+++ tp6/main.cu
@@ -41,13 +41,13 @@ void scan_gpu1(int* x)
 
     for (int offset = 1; offset < T; offset*=2)
     {
-        if (offset < i)
+        if (offset <= i)
         {
             buffers[i + T] = buffers[i];
         }
         else
         {
-            //buffers[i + offset + T] += buffers[i];
+            buffers[i + offset + T] += buffers[i];
         }
         __syncthreads();
         buffers[i] = buffers[i+T];
