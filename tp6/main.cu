f2cdf7514bd1b09a4222a91b547d997bf77a5edb
alexandre di-santo <alexandre.di-santo@epita.fr>
2024-11-04 09:25:46 +0100

    bridged

--- tp6/main.cu
+++ tp6/main.cu
@@ -14,9 +14,9 @@ std::vector<int> scan_cpu(const std::vector<int>& x)
 {
     std::vector<int> res = std::vector<int>(x.size());
     res[0] = 0;
-    for (int i = 1; i < x.size() - 1; i++)
+    for (int i = 1; i < x.size()-1; i++)
     {
-        res[i] = res[i-1] + x[i];
+        res[i] = res[i-1] + x[i-1];
     }
     return res;
 }
