08bff135c1b27fee15e340db16a160c86ead92b1
alexandre di-santo <alexandre.di-santo@epita.fr>
2024-11-04 10:54:42 +0100

    bridged

---
+++ tp6/README.md
@@ -0,0 +1,5 @@
+### Q1 
+
+Quelle est la complexité de cet algorithme en fonction de la taille T du tableau ?  
+La complexite est de log2(T)
+
--- tp6/main.cu
+++ tp6/main.cu
@@ -33,10 +33,7 @@ void scan_gpu1(int* x)
     int i = threadIdx.x;
 
     buffers[i] = x[i];
-    // if (i != 0)
-    // {
-    //     buffers[i] += x[i-1];
-    // }
+
     __syncthreads();
 
     for (int offset = 1; offset < T; offset*=2)
