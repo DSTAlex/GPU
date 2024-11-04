534f998679de37a08db1ff9417b1c387743ded26
alexandre di-santo <alexandre.di-santo@epita.fr>
2024-11-04 15:27:08 +0100

    bridged

--- tp6/main.cu
+++ tp6/main.cu
@@ -99,7 +99,8 @@ void scan_gpu2(int* x)
     for (int offset = 2; offset < 3; offset*=2)
     {
         if (i * offset - offset +1 > 0)
-            x[i * offset + 1] += x[i * offset +1- offset];
+            //x[i * offset + 1] += x[i * offset +1- offset];
+            x[i * offset + 1] = 0;
     }
 
 }
