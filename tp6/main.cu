acf3d3cf46940bfb983b673b8a70f531323c8660
alexandre di-santo <alexandre.di-santo@epita.fr>
2024-11-04 11:16:10 +0100

    bridged

--- tp6/main.cu
+++ tp6/main.cu
@@ -227,7 +227,7 @@ int main()
         const std::vector<int> y_test = scan_gpu2<8>(x);
         if(y_test != y_sol) {
             std::cout << "failure" << std::endl;
-            d::cout << "  expected = [0,3,5,10,16,24,31,35]" << std::endl;
+            std::cout << "  expected = [0,3,5,10,16,24,31,35]" << std::endl;
             std::cout << "  get      = [";
             for(int val : y_test) std::cout << val << ",";
             std::cout << "]" << std::endl;
@@ -242,7 +242,7 @@ int main()
         const std::vector<int> y_test = scan_gpu2<16>(x);
         if(y_test != y_sol) {
             std::cout << "failure" << std::endl;
-            d::cout << "  expected = [0,16,31,35,49,59,60,73,85,87,98,107,114,122,127,130]" << std::endl;
+            std::cout << "  expected = [0,16,31,35,49,59,60,73,85,87,98,107,114,122,127,130]" << std::endl;
             std::cout << "  get      = [";
             for(int val : y_test) std::cout << val << ",";
             std::cout << "]" << std::endl;
