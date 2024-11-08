f2090dacdcd23f94d7ba1d14e95eaf9a58b92307
alexandre di-santo <alexandre.di-santo@epita.fr>
2024-11-08 09:55:10 +0100

    bridged

--- tp6/main.cu
+++ tp6/main.cu
@@ -270,11 +270,11 @@ int main()
             std::cout << "failure" << std::endl;
 
             
-            for(int i = 0 ; i < 1024; i++) 
+            for(int i = 0 ; i < 10; i++) 
             {
                 if (y_sol[i] != y_test[i])
                 {
-                    std::cout << ' ' << i;
+                    std::cout << ' ' << i << ' ' << y_sol[i] << ' ' << y_test[i] << std::endl;
                 }
             }
             
