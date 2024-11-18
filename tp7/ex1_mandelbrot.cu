33126e30e66005cda857ce819a47b085f8d17749
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-11-18 11:36:17 +0100

    bridged

--- tp7/ex1_mandelbrot.cu
+++ tp7/ex1_mandelbrot.cu
@@ -19,7 +19,7 @@ inline void cuda_check(cudaError_t code, const char *file, int line) {
 template <typename T>
 __device__ inline T* get_ptr(T *img, int i, int j, int C, size_t pitch) 
 {
-    return img +  i * pitch/sizeof(float) + j * C;
+    return (T*)((char*)img +  i * pitch + j * C);
 }
 
 __host__ __device__
