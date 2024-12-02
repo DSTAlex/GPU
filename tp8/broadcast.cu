7cee230cec217d141870843a469c343ff42b29c5
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-12-02 10:39:26 +0100

    bridged

--- tp8/broadcast.cu
+++ tp8/broadcast.cu
@@ -14,6 +14,12 @@ __global__ void broadcast1(int* x, int N)
 
 __global__ void broadcast2(int* x, int N)
 {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i>N)
+        return;
+    int y = x[i];
+    y = __shfl_sync(0xFFFFFFFF, y, 7, 8);
+    x[i] = y;
 
 }
 
