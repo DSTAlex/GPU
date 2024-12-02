7087bab296ad3caba7fd4b224b4c382f630c43e4
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-12-02 10:59:31 +0100

    bridged

--- tp8/reduce.cu
+++ tp8/reduce.cu
@@ -3,7 +3,11 @@
 
 __device__ int warp_reduce(int sum) 
 {
-
+    for (int k = 32; k >= 1; k/=2){
+        sum += __shfl_down_sync(0xFFFFFFFF, sum, k/2, 32);
+        __syncthreads();
+    }
+    return sum;
 }
 
 
@@ -11,7 +15,14 @@ namespace kernel {
 
 __global__ void reduce1(const int *x, int *y, int N) 
 {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i>N)
+        return;
+    
+    int v = warp_reduce(x[i]); 
 
+    if (i == 0)
+        y* = v;
 }
 
 __global__ void reduce2(const int *x, int *y, int N) 
