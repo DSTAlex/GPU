58db55dae3767697d8a2e123b03d5f083b8f8450
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-12-02 12:00:14 +0100

    bridged

--- tp8/reduce.cu
+++ tp8/reduce.cu
@@ -47,7 +47,7 @@ __global__ void reduce2(const int *x, int *y, int N)
     //     buffer[i / 32] = v;
     __syncthreads();
 
-    // int val = 0;
+    int val = 0;
     // if (i % 32 == 0)
     // {
     //     val = warp_reduce(v);   
