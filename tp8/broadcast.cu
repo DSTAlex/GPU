14796f272641cff74b5ae56f66e57cf1fce6f262
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-12-02 10:40:29 +0100

    bridged

--- tp8/broadcast.cu
+++ tp8/broadcast.cu
@@ -25,7 +25,12 @@ __global__ void broadcast2(int* x, int N)
 
 __global__ void broadcast3(int* x, int N)
 {
-
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i>N)
+        return;
+    int y = x[i];
+    y = __shfl_sync(0xFFFFFFFF, y, 0, 2);
+    x[i] = y;
 }
 
 } // namespace kernel
