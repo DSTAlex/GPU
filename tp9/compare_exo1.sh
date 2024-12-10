9baaa7e9e4137cf19ff73f75c23c737ba9857f65
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-12-10 11:16:40 +0100

    bridged

--- tp9/compare_exo1.sh
+++ tp9/compare_exo1.sh
@@ -1,7 +1,7 @@
 git pull
 
-nvcc ex1_2_cu --allow-unsupported-compiler --extended-lambda -o exo2
-nvcc ex1_3_cu --allow-unsupported-compiler --extended-lambda -o exo3
+nvcc ex1_2.cu --allow-unsupported-compiler --extended-lambda -o exo2
+nvcc ex1_3.cu --allow-unsupported-compiler --extended-lambda -o exo3
 
 ./exo2
 ./exo3
