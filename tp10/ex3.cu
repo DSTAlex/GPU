0e4744b028cc65a4f32d6dc0c3a20ef3853ad18e
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-12-20 14:30:39 +0100

    bridged

--- tp10/ex3.cu
+++ tp10/ex3.cu
@@ -8,7 +8,7 @@ thrust::host_vector<int> random_sample(
 {
     const int N = h_scores.size();
     thrust::device_vector<int> d_scores = h_scores; // host to device
-    
+    thrust::device_vector<float> d_proba(N);
     
     int sum = thrust::reduce(thrust::device, d_scores.begin(), d_scores.end(), 0, thrust::plus<int>());
 
@@ -17,31 +17,28 @@ thrust::host_vector<int> random_sample(
         sum = 1;
     }
 
-    thrust::transform(thrust::device, d_scores.begin(), d_scores.end(), d_scores.begin(), [sum]__device__(auto zip)->float
+    thrust::transform(thrust::device, d_scores.begin(), d_scores.end(), d_proba.begin(), [sum]__device__(auto zip)->float
         {
             return (float)zip / (float)sum;
         });
 
-    thrust::inclusive_scan(d_scores.begin(), d_scores.end(), d_scores.begin());
+    thrust::inclusive_scan(d_proba.begin(), d_proba.end(), d_proba.begin());
 
     thrust::device_vector<int> res(M);
     thrust::device_vector<float> random(M);
 
     auto iter = thrust::make_counting_iterator(0);
 
-    auto begin = thrust::make_zip_iterator(random.begin(), iter);
-    auto end = thrust::make_zip_iterator(random.end(), iter + M);
-
     thrust::transform(thrust::device, iter, iter+M, random.begin(), []__device__(auto proba)->float
         {
             return RNG()(proba);
         });
 
-    thrust::transform(thrust::device, random.begin(), random.end(), res.begin(), [d_scores]__device__(auto proba)->int
+    thrust::transform(thrust::device, random.begin(), random.end(), res.begin(), [d_proba]__device__(auto proba)->int
         {
             int index = 0;
-            printf("%f\n", proba);
-            for(auto score : d_scores){
+            printf("pb:%f, score:%i\n", proba);
+            for(auto score : d_proba){
                 if (score > proba)
                     return index;
                 index++;
