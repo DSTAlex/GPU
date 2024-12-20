6d2936bb788e0f3954ad1ce065cefa5edcb210d4
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-12-20 11:57:20 +0100

    bridged

--- tp10/ex3.cu
+++ tp10/ex3.cu
@@ -8,7 +8,7 @@ thrust::host_vector<int> random_sample(
     const int N = h_scores.size();
     thrust::device_vector<int> d_scores = h_scores; // host to device
     
-    int sum = thrust::reduce(d_scores.begin(), d_scores.end(), 0, std::plus<uint64_t>{});
+    int sum = thrust::reduce(d_scores.begin(), d_scores.end(), 0, []__device__(int a, int b){return a + b;});
 
     return {};
 }
