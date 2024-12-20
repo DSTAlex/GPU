de027da10c3ed1ad974e45b290fe6397f281f11c
alexandre.di-santo <alexandre.di-santo@epita.fr>
2024-12-20 12:03:09 +0100

    bridged

--- tp10/ex3.h
+++ tp10/ex3.h
@@ -6,3 +6,7 @@
 thrust::host_vector<int> random_sample(
     const thrust::host_vector<int>& h_scores, 
     int M);
+
+thrust::device_vector<int> random_sample_device(
+    const thrust::device_vector<int>& d_scores, 
+    int M, int N);
