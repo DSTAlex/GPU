f0c2a7f797c596ab3ac27a7a8fdeeb1ef4e81df8
alexandre di-santo <alexandre.di-santo@epita.fr>
2025-01-15 10:23:02 +0100

    bridged

---
+++ exqm_s9/ex1.cu
@@ -0,0 +1,70 @@
+#include "ex1.h"
+
+__device__ int warp_reduce_max(int m)
+{
+    int other = 0;
+    for (int k = 32; k > 1; k/=2){
+        other = __shfl_down_sync(0xFFFFFFFF, m, k/2, 32);
+        if (other > m)
+            m = other;
+        __syncthreads();
+    }
+    return m;
+}
+
+namespace kernel {
+
+__global__ void max_abs(const int *x, int N, int *y)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i > N)
+        return;
+    
+    int v = warp_reduce_max(abs(x[i])); 
+
+    if (i % 32 == 0)
+        y[i / 32] = v;
+}
+
+} // namespace kernel
+
+int max_abs_cpu(const int *x, int N)
+{
+    int result = 0;
+    for(int i = 0; i < N; ++i)
+    {
+        if(abs(x[i]) > result)
+        {
+            result = abs(x[i]);
+        }
+    }
+    return result;
+}
+
+int max_abs_gpu(const int *x, int N)
+{
+    int max = 0;
+
+    const int B = (N + T - 1) / T;
+
+    std::vector<int> y(B*W);
+
+    int* dy = nullptr;
+    CUDA_CHECK( cudaMalloc(&dy, W*B*sizeof(int)) );
+
+    int* dx = nullptr;
+    CUDA_CHECK( cudaMalloc(&dx, N*sizeof(int)) );
+    CUDA_CHECK( cudaMemcpy(dx, x, N*sizeof(int), cudaMemcpyHostToDevice) );
+
+    kernel::max_abs<<<B, T>>>(dx, N, dy);
+
+    CUDA_CHECK( cudaMemcpy(y.data(), dy, W*B*sizeof(int), cudaMemcpyDeviceToHost) );
+    CUDA_CHECK( cudaFree(dy) );
+    
+    for ( int v : y){
+        if (v > max)
+            max = v;
+    }
+
+    return max;
+}
---
+++ exqm_s9/ex2.cu
@@ -0,0 +1,57 @@
+#include "ex2.h"
+
+thrust::host_vector<int> copy_positive_cpu(const thrust::host_vector<int>& hx)
+{
+    const int N = hx.size();
+    thrust::host_vector<int> results;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (hx[i] > 0)
+        {
+            results.push_back(hx[i]);
+        }
+    }
+
+    return results;
+}
+
+struct is_ok
+{
+  __host__ __device__
+  bool operator()(int x)
+  {
+    return x == 1;
+  }
+};
+
+thrust::host_vector<int> copy_positive_gpu(const thrust::host_vector<int>& hx)
+{
+     const thrust::device_vector<int> dx = hx; // host to device
+
+    const int N = hx.size();
+    thrust::device_vector<int> is_pos(N);
+    thrust::device_vector<int> map(N);
+
+    thrust::transform(dx.begin(), dx.end(), is_pos.begin(), []__device__(auto value)->int
+        {
+            if (value > 0)
+                return 1;
+            else
+                return 0;
+        });
+
+
+
+    thrust::exclusive_scan(is_pos.begin(), is_pos.end(), map.begin());
+    
+    const int M = map[N - 1];
+
+    thrust::device_vector<int> results(M);
+
+    is_ok pred;
+
+    thrust::scatter_if(dx.begin(), dx.end(), map.begin(), is_pos.begin(), results.begin(), pred);
+
+    return map;
+}
