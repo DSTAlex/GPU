9806c5e6c4cc4b60bed873403b6df1b3ca3acdec
alexandre di-santo <alexandre.di-santo@epita.fr>
2025-01-15 09:50:57 +0100

    bridged

--- exam_s9/ex1.cu
+++ exam_s9/ex1.cu
@@ -51,12 +51,12 @@ int max_abs_gpu(const int *x, int N)
 
     int* dy = nullptr;
     CUDA_CHECK( cudaMalloc(&dy, W*B*sizeof(int)) );
-    
+
     int* dx = nullptr;
     CUDA_CHECK( cudaMalloc(&dx, N*sizeof(int)) );
     CUDA_CHECK( cudaMemcpy(dx, x.data(), N*sizeof(int), cudaMemcpyHostToDevice) );
 
-    kernel::reduce1<<<B, T>>>(dx, dy, N);
+    kernel::max_abs<<<B, T>>>(dx, dy, N);
 
     CUDA_CHECK( cudaMemcpy(y.data(), dy, W*B*sizeof(int), cudaMemcpyDeviceToHost) );
     CUDA_CHECK( cudaFree(dy) );
