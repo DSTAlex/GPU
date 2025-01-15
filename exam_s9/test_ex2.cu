ee535b7132b2c1e11348b8c5a761d77b7320c829
alexandre di-santo <alexandre.di-santo@epita.fr>
2025-01-15 10:27:59 +0100

    bridged

--- exam_s9/test_ex2.cu
+++ exam_s9/test_ex2.cu
@@ -7,7 +7,7 @@ int main()
 {
     {
         std::cout << "Test 1" << std::endl;
-        const thrust::host_vector<int> x = {1,2,-3,-4,5,6,-7,-8}; // 8 elements
+        const thrust::host_vector<int> x = read_from_file<int>("exam_s9/data.txt", 1000000);
         const thrust::host_vector<int> z_true = copy_positive_cpu(x);
         const thrust::host_vector<int> z_test = copy_positive_gpu(x);
         // if(z_true.size() != z_test.size()) {
@@ -16,26 +16,26 @@ int main()
         //     std::cerr << "  got      : " << z_test.size() << std::endl;
         //     return 0;
         // }
-        // const int N = z_true.size();
-        // for(int i = 0; i<N; ++i) {
-        //     if(z_test[i] != z_true[i]) {
-        //         std::cerr << "Error: at i=" << i << std::endl;
-        //         std::cerr << "  expected : " << z_true[i] << std::endl;
-        //         std::cerr << "  got      : " << z_test[i] << std::endl;
-        //         return 0;
-        //     }
-        // }
+        const int N = z_true.size();
+        for(int i = 0; i<N; ++i) {
+            if(z_test[i] != z_true[i]) {
+                std::cerr << "Error: at i=" << i << std::endl;
+                std::cerr << "  expected : " << z_true[i] << std::endl;
+                std::cerr << "  got      : " << z_test[i] << std::endl;
+                return 0;
+            }
+        }
         // std::cout << "Success" << std::endl;
-        std::cout << "input  ";
-        for (auto e : x)
-        {
-            std::cout << e << " ";
-        }
-        std::cout << "\noutput ";
-        for (auto e : z_test)
-        {
-            std::cout << e << " ";
-        }
-        std::cout << "\n";
-    }
+    //     std::cout << "input  ";
+    //     for (auto e : x)
+    //     {
+    //         std::cout << e << " ";
+    //     }
+    //     std::cout << "\noutput ";
+    //     for (auto e : z_test)
+    //     {
+    //         std::cout << e << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
 }
